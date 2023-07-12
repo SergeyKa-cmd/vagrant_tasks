1ab738219c6428924d6d5d1a65e16234615d5f70
Sergey <sserdj7@gmail.com>
2023-07-12 10:39:53 +0000

    correct flask

--- task2/web_service.py
+++ task2/web_service.py
@@ -1,11 +1,12 @@
 from flask import Flask, render_template, request
+import socket
 
 app = Flask(__name__)
-@app.route('/')
-@app.route('/index')
-def index():
-    ip_client = request.environ['REMOTE_ADDR']
+@app.route('/greetings')
+def greetings():
+    ip_client = request.headers.get('X-Forwarded-For')
     ip_server = request.remote_addr
     return render_template('index.html', ip_address_of_client=ip_client, server_ip=ip_server)
 if __name__ == "__main__":
-  app.run(host='0.0.0.0', port=5000)
+  ip_host = socket.gethostbyname(socket.gethostname())
+  app.run(host = ip_host, port=5000)
